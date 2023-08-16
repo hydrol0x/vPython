2933507dc9cfc448fe1ca301b7c08df531b7df2f
hydrol0x <mrjacobry@gmail.com>
2023-08-15 23:05:09 -0400

    create-particles function

--- rust/test3d/src/main.rs
+++ rust/test3d/src/main.rs
@@ -3,7 +3,7 @@ extern crate kiss3d;
 use std::array;
 use std::iter::Enumerate;
 
-use kiss3d::nalgebra::{Vector3, UnitQuaternion, Translation3};
+use kiss3d::nalgebra::{Vector3, UnitQuaternion, Translation3, ArrayStorage};
 use kiss3d::window::{Window};
 use kiss3d::scene::SceneNode;
 use kiss3d::light::Light;
@@ -11,30 +11,14 @@ use kiss3d::light::Light;
 
 fn main() {
     let mut window = Window::new("Kiss3d: cube");
-    let mut c      = window.add_cube(1.0,1.0,1.0);
-    let mut c2      = window.add_cube(0.1,0.1,0.1);
-    let mut p1 = window.add_sphere(0.5);
-    let mut p2 = p1.clone();
-    
-    let particles = [p1,p2];
-
-    let mut i:f32 = 0.0;
-
-    for mut particle in particles {
-        particle.set_color(0.0, 1.0, 0.0);
-        let trans = translate(0.0,i,0.0);
-        particle.prepend_to_local_translation(&trans);
-        i+=1.0; 
-    }
-
     window.set_light(Light::StickToCamera);
 
-    // let rot = UnitQuaternion::from_axis_angle(&Vector3::y_axis(), 0.014);
+    create_particles(3, window);
 
     while window.render() {
         // c.prepend_to_local_rotation(&rot);
-        let t : Translation3<f32> = translate(0.01,0.0,0.0);
-        c.prepend_to_local_translation(&t);
+        // let t : Translation3<f32> = translate(0.01,0.0,0.0);
+        // c.prepend_to_local_translation(&t);
     }
 }
 
@@ -42,11 +26,14 @@ fn translate(x: f32,y: f32,z: f32) -> Translation3<f32> {
     Translation3::new(x,y,z)
 }
 
-fn testprint() {
-    let nums = [1,2];
-    for num in nums{
-        println!("{}",num);
+fn create_particles(num_p: i32, mut window: Window) -> Vec<SceneNode> {
+    let mut particles = Vec::new();
+    for i in 1..num_p {
+        let particle = window.add_sphere(1.0);
+        particles.push(particle);
+        println!("Created particle {}", i);
     }
+    particles
 }
 
 
